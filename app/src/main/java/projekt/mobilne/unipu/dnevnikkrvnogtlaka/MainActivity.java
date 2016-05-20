32bca750d798a4a2c1a5cbb31562d11652002aca
mrisek <mrisek@net.hr>
2016-05-21 00:43:35 +0200

    Dodan projekt, četiri glavna screena i navigacija
    Napravljen je novi projekt u programu Android Studio. Dodane su četiri
    glavne aktivnosti, a to su redom MainActivity, PovijestActivity,
    PregledActivity i UnosActivity. Za svaki od aktivnosti implementirana je
    navigacija putem opcijskog izbornika.

---
+++ app/src/main/java/projekt/mobilne/unipu/dnevnikkrvnogtlaka/MainActivity.java
@@ -0,0 +1,63 @@
+package projekt.mobilne.unipu.dnevnikkrvnogtlaka;
+
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        View v = getWindow().getDecorView().findFocus();
+
+        if (id == R.id.povijest_menu) {
+            povijestKrvnogTlaka(v);
+            return true;
+        }
+        else if (id == R.id.unos_menu) {
+            unosNovogKrvnogTlaka(v);
+            return true;
+        }
+        else if (id == R.id.pregled_menu) {
+            pregledKrvnogTlaka(v);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void povijestKrvnogTlaka(View v) {
+        Intent i  = new Intent(MainActivity.this, PovijestActivity.class);
+        startActivity(i);
+    }
+
+    public void unosNovogKrvnogTlaka(View v) {
+        Intent i  = new Intent(MainActivity.this, UnosActivity.class);
+        startActivity(i);
+    }
+
+    public void pregledKrvnogTlaka(View v) {
+        Intent i  = new Intent(MainActivity.this, PregledActivity.class);
+        startActivity(i);
+    }
+
+}
