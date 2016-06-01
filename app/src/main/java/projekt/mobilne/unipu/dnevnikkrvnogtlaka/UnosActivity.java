3fd0bfe779945825a6217e9aa12ffc99c3089702
mrisek <mrisek@net.hr>
2016-06-02 00:57:35 +0200

    Postavljen hrvatski jezik i pametna tipkovnica

--- app/src/main/java/projekt/mobilne/unipu/dnevnikkrvnogtlaka/UnosActivity.java
+++ app/src/main/java/projekt/mobilne/unipu/dnevnikkrvnogtlaka/UnosActivity.java
@@ -27,11 +27,12 @@ public class UnosActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_unos);
 
-        // kontrole za unos parametara nakon mjerenja krvnog tlaka
+        // EditText-ovi za unos parametara nakon mjerenja krvnog tlaka
         etSistolicki = (EditText)findViewById(R.id.etSistolicki);
         etDijastolicki = (EditText)findViewById(R.id.etDijastolicki);
         etPuls = (EditText)findViewById(R.id.etPuls);
 
+        // Onemogućavanje unosa sa klasične android tipkovnice
         etSistolicki.setInputType(InputType.TYPE_NULL);
         etDijastolicki.setInputType(InputType.TYPE_NULL);
         etPuls.setInputType(InputType.TYPE_NULL);
@@ -45,22 +46,28 @@ public class UnosActivity extends AppCompatActivity {
         // Tipka za potvrdu unosa krvnog tlaka
         Button unosKrvnogTlaka = (Button)findViewById(R.id.buttonUnos);
 
+        // Tipka za brisanje znamenki unosa vrijednosti krvnog tlaka
         Button buttonBrisanje = (Button)findViewById(R.id.buttonBrisanje);
 
+        // Listener za tipku "Brisanje"
         buttonBrisanje.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                // EditText za unos SYS tlaka
                 if (etSistolicki.isFocused()) {
                     if(etSistolicki.length() > 0 )
                         etSistolicki.setText(etSistolicki.getText().toString().substring(0, etSistolicki.length()-1));
-                } else if (etDijastolicki.isFocused()) {
+                }
+                // EditText za unos DIA tlaka
+                else if (etDijastolicki.isFocused()) {
                     if(etDijastolicki.length() > 0 ) {
                         etDijastolicki.setText(etDijastolicki.getText().toString().substring(0, etDijastolicki.length() - 1));
                         if (etDijastolicki.length() == 0)
                             etSistolicki.requestFocus();
                     }
-                } else if (etPuls.isFocused()) {
+                }
+                // EditText za unos PLS-a
+                else if (etPuls.isFocused()) {
                     if(etPuls.length() > 0 ) {
                         etPuls.setText(etPuls.getText().toString().substring(0, etPuls.length() - 1));
                         if(etPuls.length() == 0)
@@ -70,33 +77,43 @@ public class UnosActivity extends AppCompatActivity {
             }
         });
 
+        // Listener za tipku OK
         unosKrvnogTlaka.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Ukoliko nisu prazni EditTextovi
                 if (!etSistolicki.getText().toString().equals("") &&
                         !etDijastolicki.getText().toString().equals("") &&
                         !etPuls.getText().toString().equals("")) {
 
+                    // Definicija integera koji se unose u bazu
                     int sistolicki = Integer.parseInt(etSistolicki.getText().toString());
                     int diastolicki = Integer.parseInt(etDijastolicki.getText().toString());
                     int puls = Integer.parseInt(etPuls.getText().toString());
 
+                    // Instanciranje novog objekta tipa Validacija za provjeru unosa
                     Validacija validacija = new Validacija(getApplicationContext());
                     val = validacija.validiraj(sistolicki, diastolicki, puls);
+                    // Ukoliko je validacija prošla
                     if (val) {
                         /* TODO: vrijeme
                         Long vrijeme = System.currentTimeMillis()/1000;
                         String temp = myDb.getDatumIVrijeme(vrijeme);
                         */
+                        // Dodavanje novog unosa u bazu
                         dodajNoviUnos(sistolicki, diastolicki, puls);
                         View v = getWindow().getDecorView().findFocus();
                         Toast.makeText(getApplicationContext(), "Tlak je uspješno unesen", Toast.LENGTH_SHORT).show();
+                        // Otvara se screen Krvni tlak nakon uspješnog unosa
                         pocetnaStranica(v);
                     }
-                    else
-                        Toast.makeText(getApplicationContext(), "Došlo je do pogreške", Toast.LENGTH_SHORT).show();
+                    else {
+                        // Validacija nije prošla radi neregularnih vrijednosti unosa krvnog tlaka
+                        Toast.makeText(getApplicationContext(), "Greška prilikom unosa", Toast.LENGTH_SHORT).show();
+                    }
                 }
                 else {
+                    // Nisu popunjena sva polja EditTextova
                     Toast.makeText(getApplicationContext(), "Morate popuniti sva polja!", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -104,20 +121,26 @@ public class UnosActivity extends AppCompatActivity {
         });
     }
 
+    // region JAVNE METODE
     public void onClickNumberButton(View v) {
+
         CharSequence broj = ((Button)v).getText();
+
         if (etSistolicki.isFocused()) {
             etSistolicki.append(broj);
             if (etSistolicki.length() == 3)
                 etDijastolicki.requestFocus();
-        } else if (etDijastolicki.isFocused()) {
+        }
+        else if (etDijastolicki.isFocused()) {
             etDijastolicki.append(broj);
             if (etDijastolicki.length() == 3)
                 etPuls.requestFocus();
-        } else if (etPuls.isFocused()) {
+        }
+        else if (etPuls.isFocused()) {
             etPuls.append(broj);
         }
     }
+    // endregion
 
     // region PRIVATNE METODE
     private void dodajNoviUnos(int sistolicki, int dijastolicki, int puls) {
--- app/src/main/java/projekt/mobilne/unipu/dnevnikkrvnogtlaka/Validacija.java
+++ app/src/main/java/projekt/mobilne/unipu/dnevnikkrvnogtlaka/Validacija.java
@@ -6,20 +6,29 @@ import android.widget.Toast;
 
 public class Validacija {
 
+    // region PRIVATNE VARIJABLE
     private Context context;
-    private final int SISTOLICKI_MAX = 155;
+    private final int SISTOLICKI_MAX = 200;
+    private final int DIJASTOLICKI_MAX = 200;
+    private final int PULS_MAX = 200;
+    private final int SISTOLICKI_MIN = 1;
+    private final int DIJASTOLICKI_MIN = 1;
+    private final int PULS_MIN = 1;
+    // endregion
 
+    // region JAVNE METODE
     public Validacija(Context context) {
         this.context = context;
     }
 
     public boolean validiraj(int sistolicki, int diastolicki, int puls) {
 
-        if(sistolicki > 200 || diastolicki > 200 || puls > 200 || sistolicki < 1 || diastolicki < 1 || puls < 1) {
-            Toast.makeText(context, "Greška prilikom unosa!", Toast.LENGTH_SHORT).show();
-
+        if(sistolicki > SISTOLICKI_MAX || diastolicki > DIJASTOLICKI_MAX || puls > PULS_MAX || sistolicki < SISTOLICKI_MIN || diastolicki < DIJASTOLICKI_MIN || puls < PULS_MIN) {
+            // Evidentiranje pogreške prilikom unosa
             return false;
         }
+        // Unos je ispravan
         return true;
     }
+    // endregion
 }
